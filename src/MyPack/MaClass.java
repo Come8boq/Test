31d199c9a30589d3f0276bc0451c994b108d60df
Come8boq <beauquiercome@gmail.com>
2024-11-13 19:16:03 +0100

    Commit Test

---
+++ src/MyPack/MaClass.java
@@ -0,0 +1,13 @@
+package MyPack;
+
+public class MaClass {
+
+	public MaClass() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
